25e29189ffdce1096e20bf8b9b92d0fc7027c06e
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2021-03-16 22:04:28 +0100

    WorkHours

--- src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
@@ -1,4 +1,21 @@
 package hu.nive.ujratervezes.zarovizsga.workhours;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class WorkHours {
+    public String minWork(String file) {
+        Path fileother = Path.of(file);
+        try {
+            List<String> employees = Files.readAllLines(fileother);
+            for (String employee: employees) {
+
+            }
+        }
+        catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
 }
